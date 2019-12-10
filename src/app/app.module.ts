d46d1b16128ab39436d289009f8e04758bda1060
Ravindra Dhore <ravindrad@ansh.com>
2019-12-10 14:39:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { QrCodeModule } from 'ng-qrcode';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QrCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
